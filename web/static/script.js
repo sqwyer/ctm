5afe520e43df720e89982f908c3d79b7cd96fed4
Sawyer Bivens <570000826@nlrsd.org>
2022-04-28 10:46:26 -0500

    Continue on web

--- web/static/script.js
+++ web/static/script.js
@@ -19,7 +19,7 @@ function popup(options) {
     if(title && title != null) {
         let section = _div();
         let content = _p();
-        content.innerText = title;
+        content.innerHTML = title;
         content.className = 'title'
         section.appendChild(content);
         div.appendChild(section);
@@ -28,7 +28,7 @@ function popup(options) {
     if(message && message != null) {
         let section = _div();
         let content = _p();
-        content.innerText = message;
+        content.innerHTML = message;
         section.appendChild(content);
         div.appendChild(section);
     }
@@ -39,7 +39,6 @@ function popup(options) {
         for(let i = 0; i < buttons.length; i++) {
             let btn = document.createElement('button');
             if(buttons[i].type) {
-                console.log(buttons[i].type)
                 switch (buttons[i].type) {
                     case 'exit':
                         btn.className = 'btn-exit';
@@ -69,13 +68,68 @@ function popup(options) {
     document.body.appendChild(div);
 }
 
-popup({
-    title: 'Some title',
-    message: 'Hello there, this is a test popup!',
-    buttons: [
-        {type: 'ok', text: 'Hi!', class: 'btn-ok', action: () => {
-            alert('hi!');
-        }},
-        {type: 'exit', text: 'Close'}
-    ]
-});
+// popup({
+//     title: 'Some title',
+//     message: 'Hello there, this is a test popup!',
+//     buttons: [
+//         {type: 'ok', text: 'Hi!', class: 'btn-ok', action: () => {
+//             alert('hi!');
+//         }},
+//         {type: 'exit', text: 'Close'}
+//     ]
+// });
+
+let playersElem = document.getElementById('players');
+
+function clickOnElem(elem) {
+    let parent = elem.parentElement;
+    let children = parent.children;
+    for(let i = 1; i < children.length-1; i++) {
+        let child = children[i];
+        let splits = child.innerText.split('\t');
+        popup({
+            title: 'Edit player',
+            message: `
+                <input placeholder="Player Name" value="${splits[0]}" type="text">
+                <input placeholder="Rating" value="${splits[1]}" type="number">
+            `,
+            buttons: [
+                {type: 'ok', text: 'Save', action: () => {
+
+                }},
+                {type: 'exit', text: 'Cancel'}
+            ]
+        })
+    }
+
+    // popup({
+    //     title: 'Some title',
+    //     message: 'Hello there, this is a test popup!',
+    //     buttons: [
+    //         {type: 'ok', text: 'Hi!', class: 'btn-ok', action: () => {
+    //             alert('hi!');
+    //         }},
+    //         {type: 'exit', text: 'Close'}
+    //     ]
+    // });
+}
+
+function addPlayer(player, rating) {
+    let tr = document.createElement('tr');
+    let tdR = document.createElement('td');
+    let tdP = document.createElement('td');
+
+    tdR.innerText = player;
+    tdR.addEventListener('click', () => clickOnElem(tdR));
+    tr.appendChild(tdR);
+
+    tdP.innerText = rating;
+    tdP.addEventListener('click', () => clickOnElem(tdP));
+    tr.appendChild(tdP);
+}
+
+for(let i = 0; i < playersElem.children[0].children.length; i++) {
+    let child = playersElem.children[0].children[i];
+
+    child.addEventListener('click', () => clickOnElem(child))
+}
